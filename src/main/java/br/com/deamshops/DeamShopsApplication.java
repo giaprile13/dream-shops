fc2cb2d1e4a6e745d01c60ddd121b3074cc69306
giaprile13 <giaprile77@gmail.com>
2024-11-02 14:52:21 -0300

    feat: first commit

---
+++ src/main/java/br/com/deamshops/DeamShopsApplication.java
@@ -0,0 +1,13 @@
+package br.com.deamshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeamShopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeamShopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/deamshops/DeamShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deamshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeamShopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
