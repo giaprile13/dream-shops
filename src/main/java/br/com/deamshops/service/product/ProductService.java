a84e52030df25dbd98a0ab68cf4ba330ca02a213
giaprile13 <giaprile77@gmail.com>
2024-11-03 20:10:18 -0300

    chore: implemented methods from interface

--- src/main/java/br/com/deamshops/service/product/ProductService.java
+++ src/main/java/br/com/deamshops/service/product/ProductService.java
@@ -1,7 +1,55 @@
 package br.com.deamshops.service.product;
 
+import br.com.deamshops.model.Category;
+import br.com.deamshops.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ProductService {
+public class ProductService implements ProductInterface {
+    @Override
+    public Product getProductById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(Category category) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrand(String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(Category category, String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrandAndName(String brand, String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndName(Category category, String name) {
+        return List.of();
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
+        return 0L;
+    }
 }
