e51049ef77f62de6840360132bd18ef67bc2058a
giaprile13 <giaprile77@gmail.com>
2024-11-03 19:47:11 -0300

    chore: implemented entities properties

--- src/main/java/br/com/deamshops/model/Image.java
+++ src/main/java/br/com/deamshops/model/Image.java
@@ -1,4 +1,29 @@
 package br.com.deamshops.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Blob;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 }
