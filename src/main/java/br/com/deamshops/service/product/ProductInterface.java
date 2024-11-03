3a10527eedf11fd4f8141ed200b2f27b9b0ef192
giaprile13 <giaprile77@gmail.com>
2024-11-03 20:10:06 -0300

    chore: implemented services methods in interface

---
+++ src/main/java/br/com/deamshops/service/product/ProductInterface.java
@@ -0,0 +1,27 @@
+package br.com.deamshops.service.product;
+
+import br.com.deamshops.model.Category;
+import br.com.deamshops.model.Product;
+
+import java.util.List;
+
+public interface ProductInterface {
+
+    Product getProductById(Long id);
+
+    void deleteProductById(Long id);
+
+    List<Product> getProductsByCategory(Category category);
+
+    List<Product> getProductsByBrand(String brand);
+
+    List<Product> getProductsByCategoryAndBrand(Category category, String brand);
+
+    List<Product> getProductsByName(String name);
+
+    List<Product> getProductsByBrandAndName(String brand, String name);
+
+    List<Product> getProductsByCategoryAndName(Category category, String name);
+
+    Long countProductsByBrandAndName(String brand, String name);
+}
--- src/main/java/br/com/deamshops/service/product/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.deamshops.service.product;
-
-import br.com.deamshops.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
