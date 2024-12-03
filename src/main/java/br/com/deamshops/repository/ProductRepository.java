f96d9784083b0a3bdb1708fc42f7ca689f5033ca
giaprile13 <giaprile77@gmail.com>
2024-12-03 11:38:09 -0300

    chore: implemented repository stereotype bean

--- src/main/java/br/com/deamshops/repository/ProductRepository.java
+++ src/main/java/br/com/deamshops/repository/ProductRepository.java
@@ -2,7 +2,8 @@ package br.com.deamshops.repository;
 
 import br.com.deamshops.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
