e5936deafc4713f0df4e4f870695d042c5299154
giaprile13 <giaprile77@gmail.com>
2024-12-17 16:39:03 -0300

    chore: created new method for adding products

--- src/main/java/br/com/deamshops/service/product/ProductInterface.java
+++ src/main/java/br/com/deamshops/service/product/ProductInterface.java
@@ -1,11 +1,12 @@
 package br.com.deamshops.service.product;
 
-import br.com.deamshops.model.Category;
 import br.com.deamshops.model.Product;
 
 import java.util.List;
 
 public interface ProductInterface {
+
+    void addProduct(Product product);
 
     Product getProductById(Long id);
 
@@ -15,17 +16,17 @@ public interface ProductInterface {
 
     List<Product> getAllProducts();
 
-    List<Product> getProductsByCategory(Category category);
+    List<Product> getProductsByCategory(String category);
 
     List<Product> getProductsByBrand(String brand);
 
-    List<Product> getProductsByCategoryAndBrand(Category category, String brand);
+    List<Product> getProductsByCategoryAndBrand(String category, String brand);
 
     List<Product> getProductsByName(String name);
 
     List<Product> getProductsByBrandAndName(String brand, String name);
 
-    List<Product> getProductsByCategoryAndName(Category category, String name);
+    List<Product> getProductsByCategoryAndName(String category, String name);
 
     Long countProductsByBrandAndName(String brand, String name);
 }
