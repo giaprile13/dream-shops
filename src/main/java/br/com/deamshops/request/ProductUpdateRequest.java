0540db93154948e7156a924ed491a60fa34cce1e
giaprile13 <giaprile77@gmail.com>
2024-12-17 17:10:05 -0300

    feat: created update request class

---
+++ src/main/java/br/com/deamshops/request/ProductUpdateRequest.java
@@ -0,0 +1,17 @@
+package br.com.deamshops.request;
+
+import br.com.deamshops.model.Category;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductUpdateRequest {
+    private Long id;
+    private String name;
+    private String brand;
+    private String description;
+    private BigDecimal price;
+    private int inventory;
+    private Category category;
+}
