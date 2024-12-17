94ffd07f250d90fe5577d76c636a00e62d5ea05c
giaprile13 <giaprile77@gmail.com>
2024-12-17 17:07:20 -0300

    chore: adapted class for setting properties

--- src/main/java/br/com/deamshops/request/AddProductRequest.java
+++ src/main/java/br/com/deamshops/request/AddProductRequest.java
@@ -1,9 +1,17 @@
 package br.com.deamshops.request;
 
 import br.com.deamshops.model.Category;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
-public record AddProductRequest(Long id, String name, String brand, String description, int inventory, BigDecimal price,
-                                Category category) {
+@Data
+public class AddProductRequest {
+    private Long id;
+    private String name;
+    private String brand;
+    private String description;
+    private BigDecimal price;
+    private int inventory;
+    private Category category;
 }
