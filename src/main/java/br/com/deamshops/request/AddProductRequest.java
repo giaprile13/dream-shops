b774d8053895d64d1828de862c7c98436a827447
giaprile13 <giaprile77@gmail.com>
2024-12-17 16:51:07 -0300

    chore: implemented properties to request record

--- src/main/java/br/com/deamshops/request/AddProductRequest.java
+++ src/main/java/br/com/deamshops/request/AddProductRequest.java
@@ -1,4 +1,9 @@
 package br.com.deamshops.request;
 
-public class AddProductRequest {
+import br.com.deamshops.model.Category;
+
+import java.math.BigDecimal;
+
+public record AddProductRequest(Long id, String name, String brand, String description, int inventory, BigDecimal price,
+                                Category category) {
 }
